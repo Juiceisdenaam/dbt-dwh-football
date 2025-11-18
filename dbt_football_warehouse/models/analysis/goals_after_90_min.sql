1778b78610c5e32236749609508563f1da4d8d69
Justin-van-Gemeren_bwag <justin.van.gemeren@knab.nl>
2025-11-18 15:27:02 +0100

    Analysis scored after 90 min

--- dbt_football_warehouse/models/analysis/goals_after_90_min.sql
+++ dbt_football_warehouse/models/analysis/goals_after_90_min.sql
@@ -1,17 +1,45 @@
-{{ config(
-    materialized='table'
-) }}
-
-select
-    fixture_id
-from {{ ref('fct_events') }} e
--- join {{ ref('fct_fixtures') }} f on e.fixture_id = f.fixture_id
--- join {{ ref('dim_teams') }} t on e.team_id = t.team_id
--- join {{ ref('dim_seasons') }} se on f.season_id = se.season_id
--- join {{ ref('dim_leagues') }} lg on f.league_id = lg.league_id
--- where LOWER(e.type_name) = 'goal'
--- --  and e.minute > 90
---   and lg.league_name = '{{ var("league_name", "Eredivisie") }}'
---   and se.season_name = '{{ var("season_year", "2023/2024") }}'
--- --group by t.team_name, lg.league_name, se.season_year
--- --order by goals_after_90 desc
+WITH events AS (
+    SELECT
+        e.fixture_id,
+        e.minute,
+        e.result,
+        t.team_name,
+        se.season_name,
+        lg.league_name
+        CAST(split_part(result, '-', 1) AS INT) as home_score_after,
+        CAST(split_part(result, '-', 2) AS INT) as away_score_after,
+    FROM {{ ref('fct_events') }} e
+    JOIN {{ ref('fct_fixtures') }} f on e.fixture_id = f.fixture_id
+    JOIN {{ ref('dim_teams') }} t on e.team_id = t.team_id
+    JOIN {{ ref('dim_seasons') }} se on f.season_id = se.season_id
+    JOIN {{ ref('dim_leagues') }} lg on f.league_id = lg.league_id
+    WHERE 1 = 1
+    AND e.result IS NOT NULL
+    AND e.type_name = 'Goal'
+), with_previous AS (
+    SELECT
+        *,
+        LAG(home_score_after,1,0) OVER (PARTITION BY fixture_id ORDER BY minute, event_id) AS home_score_before,
+        LAG(away_score_after,1,0) OVER (PARTITION BY fixture_id ORDER BY minute, event_id) AS away_score_before
+    FROM events
+), final AS (
+    SELECT
+        *,
+        CASE
+            WHEN ABS(home_score_before - away_score_before) <= 1 AND ABS(home_score_after - away_score_after) <= 1 THEN 1
+            ELSE 0
+        END AS impactful_goal
+)
+SELECT
+    team_name,
+    season_name,
+    league_name,
+    COUNT(1)
+FROM final
+WHERE minute >= 88
+AND impactful_goal = 1
+GROUP BY
+    team_name,
+    season_name,
+    league_name
+ORDER BY COUNT(1) DESC
